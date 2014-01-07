7167ed99a77c07763c5b5878e6d903dfbd8fb734
Vladislav Lewinn <vlewin@suse.de>
2014-01-07 11:37:45 +0100

    show message type as a label in IRC

--- config/initializers/01-irc_initializer.rb
+++ config/initializers/01-irc_initializer.rb
@@ -15,11 +15,21 @@ if ENV['RACK_ENV'] != 'test'
       def notify
         Notification::Message.all.each do |message|
           CONFIG[:irc][:channels].each do |channel|
-            if message.type == "error"
-              notification = Format(:red, "#{message.sender}: #{message.text}")
+            # Too much noise => bot.nick = message.sender
+
+            case message.type
+            when 'success'
+              notification = "\x0300,09 #{message.type.upcase} \x03 #{message.sender}: #{message.text}"
+            when 'info'
+              notification = "\x0300,12 #{message.type.upcase} \x03 #{message.sender}: #{message.text}"
+            when 'alert'
+              notification = "\x0300,04 #{message.type.upcase} \x03 #{message.sender}: #{message.text}"
+            when 'warning'
+              notification = "\x0300,07 #{message.type.upcase} \x03 #{message.sender}: #{message.text}"
             else
               notification = "#{message.sender}: #{message.text}"
             end
+
             success = Channel(channel).send(notification)
             message.destroy if success
           end
@@ -29,9 +39,9 @@ if ENV['RACK_ENV'] != 'test'
 
     bot = Cinch::Bot.new do
       configure do |c|
-        c.nick = CONFIG['irc']['nick']
-        c.server = CONFIG['irc']['server']
-        c.channels = CONFIG['irc']['channels']
+        c.nick = CONFIG[:irc][:nick]
+        c.server = CONFIG[:irc][:server]
+        c.channels = CONFIG[:irc][:channels]
         c.verbose = false
         c.plugins.plugins = [SvarogPlugin]
         c.log = self.loggers.first
